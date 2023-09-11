bbf01f7e16d1b5c70637921d188d963d450105bf
Mateusz Kończal <mateuszkonczal96@gmail.com>
2023-09-11 19:16:47 +0200

    2.1 Pattern and Matcher

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,9 +1,16 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class FirstTest {
 
     @Test
     void test() {
+        Pattern pattern = Pattern.compile("regex");
+        Matcher matcher = pattern.matcher("Badany ciąg znaków");
+//        matcher.find();       // Attempts to find the next subsequence of the input sequence that matches the pattern.
+//        matcher.matches();    // Attempts to match the entire region against the pattern.
 
     }
 }
